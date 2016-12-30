1c215ebad6ba72e5ae335b8eddf57f7f1b8954c5
talex0909 <malibu999francois@gmail.com>
2016-12-30 11:53:30 +0200

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,411 @@
+<!DOCTYPE html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <!-- Latest compiled and minified CSS -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+<link href="assets/css/styles.css" rel="stylesheet">
+
+<link href="assets/BANKGOTHICREGULARDB.css" rel="stylesheet">
+
+<!-- Latest compiled and minified JavaScript -->
+
+<style type="text/css">
+  
+  
+.butonnn3{ font-size:16px;
+ color:#000;
+text-decoration: none;
+transition:,font-size 0.5s, color 0.7s;
+}
+.butonnn3:hover {font-size:22px; 
+color:#dedede;
+text-decoration: none; 
+  transform: rotateX(360deg);
+transition: font-size 0.5s,color 0.5s,transform 0.8s;
+
+}
+.butonnn4{
+ font-size:16px;  color:#00b3ff;
+text-decoration: none;
+transition: color 0.7s;
+}
+.butonnn4:hover { font-size:22px; 
+color:#dedede;
+text-decoration: none;
+transition: color 0.2s;
+text-decoration: none; 
+transform: rotateX(360deg);
+transition: font-size 0.5s,color 0.5s,transform 0.8s;
+
+}
+a .show-ctrl{
+    display: none;
+}
+a .hide-ctrl{
+    display: block;
+}
+a.collapsed .show-ctrl{
+    display: block;
+}
+a.collapsed .hide-ctrl{
+    display: none;
+}
+a.show-ctrl{
+    display: none;
+}
+a .hide-ctrl{
+    display: block;
+}
+a.collapsed .show-ctrl{
+    display: block;
+}
+a .collapsed .hide-ctrl{
+    display: none;
+}
+
+
+.sociall1{
+	height:50px;
+	width:45px;
+	transition: width 0.5s ease;
+}
+.sociall1:hover{
+	width:70px;
+	transition: width 0.5s ease;
+}
+.sociall2{
+	height:50px;
+	width:45px;
+	transition: width 0.5s ease;
+}
+.sociall2:hover{
+	width:70px;
+	transition: width 0.5s ease;
+}
+.sociall3{
+	height:50px;
+	width:45px;
+	transition: width 0.5s ease;
+}
+.sociall3:hover{
+	width:70px;
+	transition: width 0.5s ease;
+}
+.sociall4{
+	height:50px;
+	width:45px;
+	transition: width 0.5s ease;
+}
+.sociall4:hover{
+	width:70px;
+	transition: width 0.5s ease;
+}
+
+.brd{
+  border-right:1px solid #000;
+  
+}
+.col-xs-12 .brd {
+  border-right:0px solid #000;
+  
+}
+@media(max-width: 768px){
+.brd{
+  border-right:0px solid #000;
+  
+
+}
+.scl2{
+	position:fixed;
+	bottom:0px;
+	width:100%;
+
+
+}
+.scl1{
+
+}
+.scl{display: none;}
+.scla{display: none;}
+}
+@media(min-width: 768px){
+
+.scl1{display: none;}
+.scl2{display: none;}
+.scla{
+	position:fixed; top:50%; left:0px;width:70px; align-content: right;
+}
+
+}
+.effb{
+	margin-top: 0px;
+	transition: margin-top 0.5s ease;
+}
+.effb:hover{
+	margin-top: -20px;
+	transition: margin-top 0.5s infinite;
+}
+
+</style>
+
+</head>
+<body style="background-color: #f9f9f9; font-family: BANKGOTHICREGULARDB;"> 
+<nav class="navbar navbar-inverse"></nav>
+<div class="scla" style="" align="left">
+	
+<div id="demo" class="collapse in" style="">
+<div class="sociall1" align="right" style="background-color: #3b5998;font-size:40px;padding-top:10px;  text-align: center; font-family: helvetica; color:#FFF;">
+	<div><b>f</b></div>
+</div>
+<div class="sociall2" style="background-color: #dd4b39;font-size:30px; color:#fff; padding-top:8px; text-align: center;">
+	<span class="glyphicon glyphicon-plus"></span>
+</div>
+<div class="sociall3" align="center" style="background-color: #0077FF;align-content: center;">
+	<div><img src="assets/img/twitter1.png" style="width:50px; margin-left:-4px;"></div>
+</div>
+<div class="sociall4" style="background-color: #999999;font-size:30px;padding-top:8px;  text-align: center; font-family: helvetica; color:#FFF;">
+	<span class="glyphicon glyphicon-envelope"></span>
+</div>
+</div>
+<a class="scl" data-toggle="collapse" data-target="#demo" align="left" style="text-decoration: none;">
+<span class=" show-ctrl glyphicon glyphicon-chevron-down" style="  font-size:18px;"></span>
+            <span class="hide-ctrl glyphicon glyphicon-chevron-up" style=" font-size:18px;"></span></a>
+</div>
+<div class="scl2">
+<div  style="background-color: #3b5998;font-size:20px;float:left; padding-top:3px; width: 25%;  text-align: center; font-family: helvetica; color:#FFF;">
+	<div><b>f</b></div>
+</div>
+<div style="background-color: #dd4b39;font-size:30px; color:#fff;float:left; width: 25%;  text-align: center;">
+<span class="glyphicon glyphicon-plus"></span>
+</div>
+<div align="center" style="background-color: #0077FF;align-content: center;float:left; width: 25%;">
+	<img src="assets/img/twitter1.png" style="height: 30px; margin-left:-4px;">
+</div>
+<div style="background-color: #999999;font-size:30px;float:left; width: 25%;  text-align: center; font-family: helvetica; color:#FFF;">
+	<span class="glyphicon glyphicon-envelope"></span>
+</div>
+
+
+</div>
+
+<div class="container ">
+  <div class="row"  style="margin-top:10px;">
+    <div class="col-md-2">
+    
+    </div>
+    <div class="col-md-8" >
+      <img src="assets/img/bannerbeta2.png" style="width:100%">
+    </div>
+  </div>
+  <div class="row"  style="margin-top:10px;">
+    <div class="col-md-3">
+    
+    </div>
+    <div class="col-md-6" >
+    <hr>
+    </div>
+  </div>
+  <div class="row " style="overflow: none;">
+  
+    <div class="col-md-12" align="center">
+       <a href="#"><div class="col-md-2 col-xs-12 brd"  style="">
+       <div class="butonnn3 ">HOME</div>
+      </div></a>
+      <div class="col-md-2 col-xs-12 brd" >
+        <div class="butonnn4">PRINT OFFICE   </div>
+      </div>
+      <div class="col-md-2 col-xs-12 brd" >
+        <div class="butonnn3">BLABLA  </div>
+      </div>
+      <div class="col-md-2 col-xs-12 brd" >
+         <div class="butonnn3">BLABLA  </div>
+      </div>
+      <div class="col-md-2 col-xs-12 brd" >
+        <div class="butonnn3">BLABLA  </div> 
+      </div>
+      <div class="col-md-2 col-xs-12" >
+        <div class="butonnn3"> CONTACT</div>
+      </div>
+      
+    </div>
+  </div>
+  <div class="row"  style="margin-top:10px;">
+    <div class="col-md-3">
+    
+    </div>
+    <div class="col-md-6" >
+    <hr>
+    </div>
+  </div>
+  </div>
+<div class="container cf" >
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2 scl"></div>
+      <div class="col-md-8" align="center">
+      <div class="col-md-2 scl"></div>
+        <div class="col-md-4 scl" align="center">
+          <div class="animation-element bounce-up cf" style="background-color: rgba(0,0,0,0); border:0px; width:200px; height:250px; padding:0px; border-radius: 100px 100PX 20PX 20PX;box-shadow:0px 0px;">
+      <div class="subject development" style="background-color: rgba(0,0,0,0); border:0px; width:200px; height:200px; padding:0px; border-radius: 100px ;box-shadow:0px 0px;">
+     
+       <div aign="center" style=" width:200px; height:250px; border:0px solid #000; border-radius: 100px 100PX 20PX 20PX;">
+           <img class="effb" src="assets/img/td.png" style="width:100%">
+           <span class="glyphicon glyphicon-chevron-up"></span>
+           <br>
+           TIPAR DIGITAL
+          </div> 
+        
+      </div>
+    </div>   
+        </div>
+        <div class="col-md-4 scl" align="center">
+              <div class="animation-element bounce-up cf" style="background-color: rgba(0,0,0,0); border:0px; width:200px; height:250px; padding:0px; border-radius: 100px 100PX 20PX 20PX;">
+      <div class="subject development" style="background-color: rgba(0,0,0,0); border:0px; width:200px; height:250px; padding:0px; border-radius: 100px 100PX 20PX 20PX; box-shadow: 0px 0px;">
+     
+       <div aign="center" style=" width:200px; height:250px; border:0px solid #000; border-radius: 100px 100PX 20PX 20PX;">
+           <img class="effb" src="assets/img/cv.png" style="width:100%">
+           <span class="glyphicon glyphicon-chevron-up"></span>
+           <br>
+           CARTI&nbsp;DE VIZITA
+          </div> 
+        
+      </div>
+    </div>  </div>  
+    
+       <div class=" col-xs-12 scl1" align="center" style="width:100%;">
+            <div class="animation-element bounce-up cf" style="background-color: rgba(0,0,0,0); border:0px; width:100%; height:250px; padding:0px; border-radius: 10px ;margin:auto;box-shadow: 0px 0px;">
+      <div class="subject development" style="background-color: rgba(0,0,0,0); border:0px; width:100%;margin:auto; height:250px; padding:0px; border-radius: 100px 100PX 20PX 20PX;margin:auto;box-shadow: 0px 0px;">
+     
+        <div aign="center" style=" width:200px; height:250px; border:0px solid #000; border-radius: 100px 100PX 20PX 20PX;">
+           <img class="effb" src="assets/img/td.png" style="width:100%">
+           <span class="glyphicon glyphicon-chevron-up"></span>
+           <br>
+           TIPAR DIGITAL
+          </div>
+        
+      </div>
+    </div>  
+        </div>
+
+      <div class="scl1 col-xs-12" align="center" style="height: 60px;"></div>
+        <div class="scl1 col-xs-12" align="center">
+            <div class="animation-element bounce-up cf" style="background-color: rgba(0,0,0,0); border:0px; width:100%; height:250px; padding:0px; border-radius: 10px ;margin:auto;box-shadow: 0px 0px;">
+      <div class="subject development" style="background-color: rgba(0,0,0,0); border:0px; width:100%;margin:auto; height:250px; padding:0px; border-radius: 100px 100PX 20PX 20PX;margin:auto;box-shadow: 0px 0px;">
+     
+        <div aign="center" style=" width:200px; height:250px; border:0px solid #000; border-radius: 100px 100PX 20PX 20PX;">
+           <img class="effb" src="assets/img/td.png" style="width:100%">
+           <span class="glyphicon glyphicon-chevron-up"></span>
+           <br>
+           CARTI DE VIZITA
+          </div>
+        
+      </div>
+    </div> </div>  
+    </div>
+       
+       
+      </div>
+     
+    </div>
+    </div>
+    <div class="container cf" >
+    <div class="row">
+      <div class="col-md-2 scl"></div>
+      <div class="col-md-8">
+        <div class="col-md-4 scl" align="center">
+           <div class="animation-element bounce-up cf" style="background-color: rgba(0,0,0,0); border:0px; width:200px; height:250px; padding:0px;  border-radius:100px 100PX 20PX 20PX ;">
+      <div class="subject development" style="background-color: rgba(0,0,0,0); border:0px; width:200px; height:250px; padding:0px;  border-radius: 100px 100PX 20PX 20PX; box-shadow: 0px 0px;">
+     
+       <div aign="center" style=" width:200px; height:250px; border:0px solid #000; border-radius: 100px 100PX 20PX 20PX;">
+           
+            <img class="effb" src="assets/img/cp.png" style="width:100%">
+           <span class="glyphicon glyphicon-chevron-up"></span>
+            <br>CANI&nbsp;PERSONALIZATE
+          
+          </div> 
+        
+      </div>
+    </div>  
+        </div>
+        <div class="col-md-4 scl" align="center">
+          <div class="animation-element bounce-up cf" style="background-color: rgba(0,0,0,0); border:0px; width:200px;border-radius: 100px 100PX 20PX 20PX; height:250px; padding:0px;  ">
+      <div class="subject development" style="background-color: rgba(0,0,0,0); border:0px; width:200px; height:250px; padding:0px;border-radius: 100px 100PX 20PX 20PX;   box-shadow: 0px 0px;">
+     
+       <div aign="center" style=" width:200px; height:250px;border-radius: 100px 100PX 20PX 20PX; border:0px solid #000;">
+            
+            <img class="effb" src="assets/img/foto.png" style="width:100%">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+           <br>FOTO
+          
+          </div> 
+        
+      </div>
+    </div>  
+        </div>
+        <div class="col-md-4 scl" align="center">
+         <div class="animation-element bounce-up cf" style="background-color: rgba(0,0,0,0); border:0px; width:200px; height:250px;border-radius: 100px 100PX 20PX 20PX; padding:0px; ">
+      <div class="subject development" style="background-color: rgba(0,0,0,0); border:0px; width:200px; height:250px; padding:0px;border-radius: 100px 100PX 20PX 20PX;  box-shadow: 0px 0px;">
+     
+       <div aign="center" style=" width:200px;border-radius: 100px 100PX 20PX 20PX; height:250px; border:0px solid #000; ">
+           
+            <img class="effb" src="assets/img/wd.png" style="width:100%">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+            <br>WEB&nbsp;DESIGN
+          
+          </div> 
+        
+      </div>
+    </div>  
+        </div>
+         <div class="col-md-12 scl1" align="center">
+           <div class="animation-element bounce-up cf" style="background-color: rgba(0,0,0,0); border:0px; width:100%; height:250px; padding:0px; border-radius: 10px ;margin:auto;box-shadow: 0px 0px;">
+      <div class="subject development" style="background-color: rgba(0,0,0,0); border:0px; width:100%;margin:auto; height:250px; padding:0px; border-radius: 100px 100PX 20PX 20PX;margin:auto;box-shadow: 0px 0px;">
+        <div aign="center" style=" width:200px; height:250px; border:0px solid #000; border-radius: 100px 100PX 20PX 20PX;">
+           <img class="effb" src="assets/img/td.png" style="width:100%">
+           <span class="glyphicon glyphicon-chevron-up"></span>
+           <br>
+           CANI PERSONALIZATE
+          </div>
+            </div>   
+        </div>
+        <div class="scl1 col-xs-12" align="center" style="height: 60px;"></div>
+        <div class="col-md-12 scl1" align="center">
+          <div class="animation-element bounce-up cf" style="background-color: rgba(0,0,0,0); border:0px; width:100%; height:250px; padding:0px; border-radius: 10px ;margin:auto;box-shadow: 0px 0px;">
+      <div class="subject development" style="background-color: rgba(0,0,0,0); border:0px; width:100%;margin:auto; height:250px; padding:0px; border-radius: 100px 100PX 20PX 20PX;margin:auto;box-shadow: 0px 0px;">
+     
+         <div aign="center" style=" width:200px; height:250px; border:0px solid #000; border-radius: 100px 100PX 20PX 20PX;">
+           <img class="effb" src="assets/img/td.png" style="width:100%">
+           <span class="glyphicon glyphicon-chevron-up"></span>
+           <br>
+           TIPAR DIGITAL
+          </div>        
+      </div>
+    </div>
+        </div>
+
+        <div class="scl1 col-xs-12" align="center" style="height: 60px;"></div>
+        <div class="col-md-12 scl1" align="center">
+          <div class="animation-element bounce-up cf" style="background-color: rgba(0,0,0,0); border:0px; width:100%; height:250px; padding:0px; border-radius: 10px ;margin:auto;box-shadow: 0px 0px;">
+      <div class="subject development" style="background-color: rgba(0,0,0,0); border:0px; width:100%;margin:auto; height:250px; padding:0px; border-radius: 100px 100PX 20PX 20PX;margin:auto;box-shadow: 0px 0px;">
+       <div aign="center" style=" width:200px; height:250px; border:1px solid #000; border-radius: 100px 100PX 20PX 20PX;">
+           <img src="assets/img/wd.png" style="width:100%">
+           <br><br>
+           WEB DESIGN
+          </div> 
+        
+      </div>
+     </div>
+            </div> 
+        </div>
+      </div>
+    </div>
+  </div>
+ </div>
+ Copiați codul următor și inserați-l într-un element <div> din secțiunea <body> a site-ului dvs., acolo unde doriți să fie afișate caseta de căutare și rezultatele căutării.
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+     <script src="assets/js/funct.js"></script>
+</body>
+</html>
